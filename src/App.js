0c17db44d5d34a8a268bf5d269ac0765362bb33c
Josh Patra <joshpatra12@gmail.com>
2023-06-30 17:37:09 -0400

    changes?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//make scrollable webpage
+
+function App(){
   return (
     <div className="App">
       <header className="App-header">
+        <h1>My First React App</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there! I was changed on the feature branch.
+          Edit <code>src/App.js</code>, save, and reload.
         </p>
         <a
+
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Now
         </a>
       </header>
     </div>
   );
 }
-
 export default App;
