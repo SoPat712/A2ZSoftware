f9928b0af0b8dbc7231ed9ecc8e8eda6434aecd7
Josh Patra <joshpatra12@gmail.com>
2023-07-02 18:05:26 -0400

    title

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ function App(){
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My First React App</h1>
+        <h1>A2Z Software</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code>, save, and reload.
         </p>
         <a
-
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
