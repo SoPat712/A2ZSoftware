4c6a387788ef55733535f0b8b3084bf0bdbcace6
Josh Patra <joshpatra12@gmail.com>
2023-07-02 18:35:05 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -9,17 +9,6 @@ function App(){
       <header className="App-header">
         <h1>A2Z Software</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>, save, and reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Now
-        </a>
       </header>
     </div>
   );
